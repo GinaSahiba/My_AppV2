ff76ad8f972082662c364d2de4acb1a4df24cac5
Gina <ginasahiba@gmail.com>
2021-11-24 14:05:03 +0700

    Evaluasi

---
+++ app/src/main/java/com/example/myappv2/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.myappv2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnKonversiRupiah: Button = findViewById(R.id.btn_konversi_rupiah)
+        btnKonversiRupiah.setOnClickListener(this)
+
+        val btnKonversiSuhu: Button = findViewById(R.id.btn_konversi_suhu)
+        btnKonversiSuhu.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_konversi_rupiah -> {
+                val moveKonversiRupiah = Intent(this@MainActivity, KonversiRupiah::class.java)
+                startActivity(moveKonversiRupiah)
+            }
+            R.id.btn_konversi_suhu -> {
+                val moveKonversiSuhu = Intent(this@MainActivity, KonversiSuhu::class.java)
+                startActivity(moveKonversiSuhu)
+            }
+        }
+    }
+}
